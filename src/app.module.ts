e28fe5867b6d843625e219deaf21a50478b1375f
huiskellyhu <kellyhu96@gmail.com>
2026-07-28 00:06:26 -0700

    feat: setup events and user files - includes some DB setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EventsModule } from './events/events.module';
+import { UsersModule } from './users/users.module';
+import { Event } from './events/entities/event.entity';
+import { User } from './users/entities/user.entity';
 
 @Module({
-  imports: [],
+  imports: [EventsModule, UsersModule, 
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: 5432,
+      username: process.env.DB_USER || 'postgres',
+      password: process.env.DB_PASSWORD || 'postgres',
+      database: process.env.DB_NAME || 'events_db',
+      entities: [Event, User],
+      synchronize: true,
+    }),],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  
+}
+
