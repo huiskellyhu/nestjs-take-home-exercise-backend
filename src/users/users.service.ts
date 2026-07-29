e1b17c6821bef7040f2bebbd5c1e0e2919d64b32
huiskellyhu <kellyhu96@gmail.com>
2026-07-28 20:48:26 -0700

    feat: add create user functionality

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private userRepo: Repository<User>,
+  ) {}
+
+  async create(dto: CreateUserDto): Promise<User> {
+    const user = this.userRepo.create({ name: dto.name });
+    return this.userRepo.save(user);
+  }
+
+  async findOne(id: string): Promise<User> {
+    const user = await this.userRepo.findOne({
+      where: { id },
+      relations: { events: true },
+    });
+    if (!user) {
+      throw new NotFoundException(`User ${id} not found`);
+    }
+    return user;
+  }
+}
